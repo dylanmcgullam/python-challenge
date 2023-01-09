20f64f64f86e5af6a752b7abfe9bd22b518db486
dylanmcgullam <119875468+dylanmcgullam@users.noreply.github.com>
2023-01-09 15:56:42 -0600

    Add files via upload

---
+++ PyBank/pybankmain.py
@@ -0,0 +1,76 @@
+import os
+import csv
+
+
+
+csvpath = os.path.join(".", "Instructions", "PyBank/Resources", "budget_data.csv")
+
+
+total_months = 0
+total_revenue =0
+period_changes =[]
+date_count = []
+greatest_increase = 0
+greatest_increase_month = 0
+greatest_decrease = 0
+greatest_decrease_month = 0
+
+
+with open(csvpath, newline = '') as csvfile:
+    csvreader = csv.reader(csvfile, delimiter = ',')
+    next(csvreader, None)
+    row = next(csvreader)
+
+    old_profit = int(row[1])
+    total_months = total_months + 1
+    total_revenue = total_revenue + int(row[1])
+    greatest_increase = int(row[1])
+    greatest_increase_month = row[0]
+
+    for row in csvreader:
+
+        total_months= total_months + 1
+        total_revenue = total_revenue + int(row[1])
+
+        revenue_changes= int(row[1]) - old_profit
+        period_changes.append(revenue_changes)
+        old_profit= int(row[1])
+        date_count.append(row[0])
+
+
+        if int(row[1]) > greatest_increase:
+            greatest_increase = int(row[1])
+            greatest_increase_month = row[0]
+
+
+        if int(row[1]) < greatest_decrease:
+            greatest_decrease = int(row[1])
+            greatest_decrease_month = row[0]
+
+
+mean_change = sum(period_changes)/len(period_changes)
+
+
+high = max(period_changes)
+low = min(period_changes)
+
+print("Financial Analysis")
+
+print("Total Months:" + str(total_months))
+print("Total Amount:" + str(total_revenue))
+print(mean_change)
+print(greatest_increase_month, max(period_changes))
+print(greatest_decrease_month, min(period_changes))
+    
+
+results_file = os.path.join(".", "Instructions", "PyBank/Resources", "budeget_data_results.text")
+
+with open(results_file, "w",) as txtfile:
+
+    txtfile.write("Financial Analysis")
+    
+    txtfile.write("Total Months:" + str(total_months))
+    txtfile.write("Total Amount:" + str(total_revenue))
+    txtfile.write("Average Change:" + str(mean_change))
+    txtfile.write("Greatest Increase in Profits:" + greatest_increase_month + str(high))
+    txtfile.write("Greatest Decrease in Profits:" + greatest_decrease_month + str(low))
